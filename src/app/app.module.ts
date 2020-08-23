60cf8a1903ea20583ed88ecfbc00b82f6a88f4dc
agarg <agarg@wdtablesystems.com>
2020-08-23 14:41:44 +0530

    added html css for tile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FiltersComponent } from './dashboard/filters/filters.component';
+import { ProductListComponent } from './dashboard/product-list/product-list.component';
+import { ProductTileComponent } from './dashboard/product-list/product-tile/product-tile.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './header/search-bar/search-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    FiltersComponent,
+    ProductListComponent,
+    ProductTileComponent,
+    HeaderComponent,
+    SearchBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
